425c122869fa671aed2bcf9f102d7ec48e5f1cea
Sebastian Velasquez Useche <00011803580@academico.icesi.edu.co>
2020-02-25 12:59:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private TextView contador;
+private int c=0;
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contador=findViewById(R.id.contador);
+
+        new Thread(
+                ()->{
+
+while (true){
+    try {
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    c++;
+    runOnUiThread(
+            ()->{
+                contador.setText(""+c);
+            }
+    );
+
+
+}
+                }
+        ).start();
+
+    }
+}
